1cf2d64898c0c789ed626c6f9402d269ee1b5c2d
Garry Byrne <1garry8@gmail.com>
2018-01-10 22:10:45 +0000

    fda

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
-<?php
+<?php // Details for connecting to the database 
 $user = 'root';
 $pass = '';
 $db = 'garryb';
-$db = new mysqli('localhost', $user, $pass, $db) or die("Unable to connect");
+$db = new mysqli('localhost', $user, $pass, $db) or die("Unable to connect"); // Message that is outputed if connection fails
 ?>﻿
